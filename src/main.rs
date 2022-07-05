a9bee77e85fd46bef3f5cba2a69f7c5d807bc18a
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-07-05 08:06:03 -0400

    Add ubprocess spawning

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{collections::{HashMap, HashSet}, io::Error, hash::BuildHasher};
+use std::{collections::{HashMap, HashSet}, io::Error, hash::BuildHasher, process::{ExitStatus}};
 use std::sync::{Arc, Mutex};
 
 use warp::{http::Response, Filter};
 use tokio::fs::{create_dir, metadata};
-use tokio::process::Command;
+use tokio::process::{Command, Child};
 use fnv::{FnvHashMap, FnvHashSet};
 use futures::join;
 
@@ -21,14 +21,14 @@ struct HTTPResult {
 
 struct MountStatus<T: BuildHasher> {
     mounted: HashSet<String, T>,
-    mounting: HashSet<String, T>
+    changing: HashSet<String, T>
 }
 
 #[tokio::main]
 async fn main() {
     let mut mount_status = MountStatus {
         mounted: FnvHashSet::default(),
-        mounting: FnvHashSet::default()
+        changing: FnvHashSet::default()
     };
     //mount_status.mounted.insert("test".to_owned());
     let already_mounted = Arc::new(Mutex::new(mount_status));
@@ -52,21 +52,22 @@ async fn mount_device<T: BuildHasher>(device_name: &str, shared_status: Arc<Mute
     let devpath = DEV_LOCATION.to_owned() + device_name;
     let first_mp = "/tmp/".to_owned() + device_name;
     let second_mp = first_mp.clone() + ".fuzzy";
+    let content = second_mp.clone() + "/content";
     match shared_status.lock() {
         Ok(mut mount_status) => {
-            if mount_status.mounted.contains(&second_mp) {
+            if mount_status.mounted.contains(&content) {
                 return HTTPResult {
                     status: 304,
                     message: "Device is already mounted.".to_owned()
                 }
             }
-            if mount_status.mounting.contains(&second_mp) {
+            if mount_status.changing.contains(&content) {
                 return HTTPResult {
                     status: 409,
-                    message: "Device mount already in progress.".to_owned()
+                    message: "Mount operation already in progress.".to_owned()
                 }
             }
-            mount_status.mounting.insert(second_mp.clone());
+            mount_status.changing.insert(content.clone());
         }, Err(_) => {
             return HTTPResult {
                 status: 500,
@@ -75,7 +76,7 @@ async fn mount_device<T: BuildHasher>(device_name: &str, shared_status: Arc<Mute
         }
     }
     if metadata(&devpath).await.is_err() {
-        if let Some(err) = remove_mounting(&second_mp, shared_status) {
+        if let Some(err) = remove_changing(&content, &shared_status) {
             return err;
         }
         return HTTPResult {
@@ -83,10 +84,9 @@ async fn mount_device<T: BuildHasher>(device_name: &str, shared_status: Arc<Mute
             message: "Requested device doesn't exist: ".to_owned() + device_name
         };
     }
-    let dirs = join!(create_dir(&first_mp),
-    create_dir(&second_mp));
+    let dirs = join!(create_dir(&first_mp), create_dir(&second_mp));
     if dirs.0.is_err() || dirs.1.is_err() {
-        if let Some(err) = remove_mounting(&second_mp, shared_status) {
+        if let Some(err) = remove_changing(&content, &shared_status) {
             return err;
         }
         return HTTPResult {
@@ -94,22 +94,81 @@ async fn mount_device<T: BuildHasher>(device_name: &str, shared_status: Arc<Mute
             message: "Could not create mountpoints.".to_owned()
         };
     }
-    Command::new("fuse-archive").arg(devpath);
+
+    // (sudo) fuse-archive /dev/sdb /tmp/sdb -o allow_other
+    let zipmount = Command::new("fuse-archive")
+    .arg(&devpath)
+    .arg(&first_mp)
+    .arg("-o")
+    .arg("allow_other")
+    .spawn();
+    if let Some(err) = handle_subprocess(zipmount, &content, &shared_status).await {
+        return err;
+    }
+
+    // (sudo) fuzzyfs /tmp/sdb /tmp/sdb.fuzzy -o allow_other
+    let fuzzymount = Command::new("fuzzyfs")
+    .arg(&first_mp)
+    .arg(&second_mp)
+    .arg("-o")
+    .arg("allow_other")
+    .spawn();
+    if let Some(err) = handle_subprocess(fuzzymount, &content, &shared_status).await {
+        return err;
+    }
+    
     return HTTPResult {
         status: 200,
         message: "OK".to_owned()
     };
 }
 
-fn remove_mounting<T: BuildHasher>(key: &str, shared_status: Arc<Mutex<MountStatus<T>>>) -> Option<HTTPResult> {
+fn remove_changing<T: BuildHasher>(key: &str, shared_status: &Arc<Mutex<MountStatus<T>>>) -> Option<HTTPResult> {
     match shared_status.lock() {
         Ok(mut mount_status) => {
-            mount_status.mounting.remove(key);
+            mount_status.changing.remove(key);
             return None;
         }, Err(_) => {
             return Some(HTTPResult {
                 status: 500,
                 message: "Failed to obtain state lock.".to_owned()
+            });
+        }
+    }
+}
+
+async fn handle_subprocess<T: BuildHasher>(spawnedproc: Result<Child, Error>, failure_key: &str, shared_status: &Arc<Mutex<MountStatus<T>>>) -> Option<HTTPResult> {
+    match spawnedproc {
+        Ok(mut child) => {
+            match child.wait().await {
+                Ok(status_code) => {
+                    if !status_code.success() {
+                        if let Some(err) = remove_changing(failure_key, shared_status) {
+                            return Some(err);
+                        }
+                        return Some(HTTPResult {
+                            status: 500,
+                            message: "Subprocess exited with an unsuccessful status.".to_owned()
+                        });
+                    }
+                    return None;
+                }, Err(_) => {
+                    if let Some(err) = remove_changing(failure_key, shared_status) {
+                        return Some(err);
+                    }
+                    return Some(HTTPResult {
+                        status: 500,
+                        message: "Could not read subprocess status.".to_owned()
+                    });
+                }
+            }
+        }, Err(_) => {
+            if let Some(err) = remove_changing(failure_key, shared_status) {
+                return Some(err);
+            }
+            return Some(HTTPResult {
+                status: 500,
+                message: "Could not spawn subprocess.".to_owned()
             });
         }
     }
